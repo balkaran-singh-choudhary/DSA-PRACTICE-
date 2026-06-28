20a36cde158e80c58cdba2133444f7335bc20961
Balkaran Singh Choudhary <balkaransingh7812@gmail.com>
2026-06-28 21:49:56 +0530

    some basic functional recursion

---
+++ recursion/recursion2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int  sum(int n){
+    if ( n == 0){
+        return 0;
+    }
+    else{
+        return n + sum(n-1);
+    }
+
+}
+int factorial(int n){
+    if ( n <=1){
+        return 1;
+    }
+    else{
+        return n *  factorial(n-1);
+    }
+
+}
+int main () {
+    int n;
+    cin>>n;
+    cout<<factorial(n)<<endl;
+
+}
