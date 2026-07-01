b1b104330cc579d41ab6fb2b6f91fe2b1e009442
Balkaran Singh Choudhary <balkaransingh7812@gmail.com>
2026-07-01 20:24:51 +0530

    hashing problem 3

---
+++ hashing/hashing3.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for ( int i = 0 ; i < n ; i++){
+        cin>>arr[i];
+    }
+    int hash[100] = {0};
+    for(int i =0 ; i<n ; i++){
+        hash[arr[i]] = 1;
+    }
+    int q;
+    cin>>q ;
+    while(q--) {
+        int numbers;
+        cin>>numbers;
+        if(hash[numbers] == 1 ) {
+            cout<<"YES"<<endl;
+        }
+        else {
+            cout<<"NO"<<endl;
+        }
+
+    }
+}
