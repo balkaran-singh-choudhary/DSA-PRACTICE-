d853f3b5a733a71b052b7967cd2cd382acc1f5b5
Balkaran Singh Choudhary <balkaransingh7812@gmail.com>
2026-07-13 20:13:11 +0530

    find element that appears one

---
+++ Arrays/array10.cpp
@@ -0,0 +1,24 @@
+//Find element that appears one
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int xorr = 0;
+
+    for (int i = 0; i < n; i++) {
+        xorr = xorr ^ arr[i];
+    }
+
+    cout << xorr << endl;
+
+    return 0;
+}
